2b5cff5971ade61ffe362093e6f1dc24d4a427e5
KeremVona <keremvona@gmail.com>
2025-10-18 13:04:20 +0300

    upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/portfolio-2",
   optimizeDeps: {
     exclude: [
       "react-pdf/dist/esm/Page/AnnotationLayer.css",
